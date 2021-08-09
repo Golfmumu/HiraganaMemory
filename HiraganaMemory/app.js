fda48762b0b45752366c77b2d47a8760d1aed56e
Golfmumu <87520312+Golfmumu@users.noreply.github.com>
2021-08-09 08:55:24 +0700

    Add files via upload

---
+++ HiraganaMemory/app.js
@@ -0,0 +1,506 @@
+const QA = [
+    {
+        id: 1,
+        ask: "あ",
+        answer: "a",
+    },
+    {
+        id: 2,
+        ask: "い",
+        answer: "i",
+    },
+    {
+        id: 3,
+        ask: "う",
+        answer: "u",
+    },
+    {
+        id: 4,
+        ask: "え",
+        answer: "e",
+    },
+    {
+        id: 5,
+        ask: "お",
+        answer: "o",
+    },
+    {
+        id: 6,
+        ask: "か",
+        answer: "ka",
+    },
+    {
+        id: 7,
+        ask: "き",
+        answer: "ki",
+    },
+    {
+        id: 8,
+        ask: "く",
+        answer: "ku",
+    },
+    {
+        id: 9,
+        ask: "け",
+        answer: "ke",
+    },
+    {
+        id: 10,
+        ask: "こ",
+        answer: "ko",
+    },
+    {
+        id: 11,
+        ask: "さ",
+        answer: "sa",
+    },
+    {
+        id: 12,
+        ask: "し",
+        answer: "shi",
+    },
+    {
+        id: 13,
+        ask: "す",
+        answer: "su",
+    },
+    {
+        id: 14,
+        ask: "せ",
+        answer: "se",
+    },
+    {
+        id: 15,
+        ask: "そ",
+        answer: "so",
+    },
+    {
+        id: 16,
+        ask: "た",
+        answer: "ta",
+    },
+    {
+        id: 17,
+        ask: "ち",
+        answer: "chi",
+    },
+    {
+        id: 18,
+        ask: "つ",
+        answer: "tsu",
+    },
+    {
+        id: 19,
+        ask: "て",
+        answer: "te",
+    },
+    {
+        id: 20,
+        ask: "と",
+        answer: "to",
+    },
+    {
+        id: 21,
+        ask: "な",
+        answer: "na",
+    },
+    {
+        id: 22,
+        ask: "に",
+        answer: "ni",
+    },
+    {
+        id: 23,
+        ask: "ぬ",
+        answer: "nu",
+    },
+    {
+        id: 24,
+        ask: "ね",
+        answer: "ne",
+    },
+    {
+        id: 25,
+        ask: "の",
+        answer: "no",
+    },
+    {
+        id: 26,
+        ask: "は",
+        answer: "ha",
+    },
+    {
+        id: 27,
+        ask: "ひ",
+        answer: "hi",
+    },
+    {
+        id: 28,
+        ask: "ふ",
+        answer: "fu",
+    },
+    {
+        id: 29,
+        ask: "へ",
+        answer: "he",
+    },
+    {
+        id: 30,
+        ask: "ほ",
+        answer: "ho",
+    },
+    {
+        id: 31,
+        ask: "ま",
+        answer: "ma",
+    },
+    {
+        id: 32,
+        ask: "み",
+        answer: "mi",
+    },
+    {
+        id: 33,
+        ask: "む",
+        answer: "mu",
+    },
+    {
+        id: 34,
+        ask: "め",
+        answer: "me",
+    },
+    {
+        id: 35,
+        ask: "も",
+        answer: "mo",
+    },
+    {
+        id: 36,
+        ask: "や",
+        answer: "ya",
+    },
+    {
+        id: 37,
+        ask: "ゆ",
+        answer: "yu",
+    },
+    {
+        id: 38,
+        ask: "よ",
+        answer: "yo",
+    },
+    {
+        id: 39,
+        ask: "ら",
+        answer: "ra",
+    },
+    {
+        id: 40,
+        ask: "り",
+        answer: "ri",
+    },
+    {
+        id: 41,
+        ask: "る",
+        answer: "ru",
+    },
+    {
+        id: 42,
+        ask: "れ",
+        answer: "re",
+    },
+    {
+        id: 43,
+        ask: "ろ",
+        answer: "ro",
+    },
+    {
+        id: 44,
+        ask: "わ",
+        answer: "wa",
+    },
+    {
+        id: 45,
+        ask: "を",
+        answer: "wo",
+    },
+    {
+        id: 46,
+        ask: "ん",
+        answer: "n",
+    },
+    {
+        id: 47,
+        ask: "が",
+        answer: "ka",
+    },
+    {
+        id: 48,
+        ask: "ぎ",
+        answer: "gi",
+    },
+    {
+        id: 49,
+        ask: "ぐ",
+        answer: "gu",
+    },
+    {
+        id: 50,
+        ask: "げ",
+        answer: "ge",
+    },
+    {
+        id: 51,
+        ask: "ご",
+        answer: "go",
+    },
+    {
+        id: 52,
+        ask: "ざ",
+        answer: "za",
+    },
+    {
+        id: 53,
+        ask: "じ",
+        answer: "ji",
+    },
+    {
+        id: 54,
+        ask: "ず",
+        answer: "zu",
+    },
+    {
+        id: 55,
+        ask: "ぜ",
+        answer: "ze",
+    },
+    {
+        id: 56,
+        ask: "ぞ",
+        answer: "zo",
+    },
+    {
+        id: 57,
+        ask: "だ",
+        answer: "da",
+    },
+    {
+        id: 58,
+        ask: "ぢ",
+        answer: "di",
+    },
+    {
+        id: 59,
+        ask: "づ",
+        answer: "zu",
+    },
+    {
+        id: 60,
+        ask: "で",
+        answer: "de",
+    },
+    {
+        id: 61,
+        ask: "ど",
+        answer: "do",
+    },
+    {
+        id: 62,
+        ask: "ば",
+        answer: "ba",
+    },
+    {
+        id: 63,
+        ask: "び",
+        answer: "bi",
+    },
+    {
+        id: 64,
+        ask: "ぶ",
+        answer: "bu",
+    },
+    {
+        id: 65,
+        ask: "べ",
+        answer: "be",
+    },
+    {
+        id: 66,
+        ask: "ぼ",
+        answer: "bo",
+    },
+    {
+        id: 67,
+        ask: "ぱ",
+        answer: "pa",
+    },
+    {
+        id: 68,
+        ask: "ぴ",
+        answer: "pi",
+    },
+    {
+        id: 69,
+        ask: "ぷ",
+        answer: "pu",
+    },
+    {
+        id: 70,
+        ask: "ぺ",
+        answer: "pe",
+    },
+    {
+        id: 71,
+        ask: "ぽ",
+        answer: "po",
+    },
+    {
+        id: 72,
+        ask: "きゃ",
+        answer: "kya",
+    },
+    {
+        id: 73,
+        ask: "きゅ",
+        answer: "kyu",
+    },
+    {
+        id: 74,
+        ask: "きょ",
+        answer: "kyo",
+    },
+    {
+        id: 75,
+        ask: "しゃ",
+        answer: "sha",
+    },
+    {
+        id: 76,
+        ask: "しゅ",
+        answer: "shu",
+    },
+    {
+        id: 77,
+        ask: "しょ",
+        answer: "sho",
+    },
+    {
+        id: 78,
+        ask: "ちゃ",
+        answer: "cha",
+    },
+    {
+        id: 79,
+        ask: "ちゅ",
+        answer: "chu",
+    },
+    {
+        id: 80,
+        ask: "ちょ",
+        answer: "cho",
+    },
+    {
+        id: 81,
+        ask: "にゃ",
+        answer: "nya",
+    },
+    {
+        id: 82,
+        ask: "にゅ",
+        answer: "nyu",
+    },
+    {
+        id: 83,
+        ask: "にょ",
+        answer: "nyo",
+    },
+    {
+        id: 84,
+        ask: "ひゃ",
+        answer: "hya",
+    },
+    {
+        id: 85,
+        ask: "ひゅ",
+        answer: "hyu",
+    },
+    {
+        id: 86,
+        ask: "ひょ",
+        answer: "hyo",
+    },
+    {
+        id: 87,
+        ask: "みゃ",
+        answer: "mya",
+    },
+    {
+        id: 88,
+        ask: "みゅ",
+        answer: "myu",
+    },
+    {
+        id: 89,
+        ask: "みょ",
+        answer: "myo",
+    },
+    {
+        id: 90,
+        ask: "りゃ",
+        answer: "rya",
+    },
+    {
+        id: 91,
+        ask: "りゅ",
+        answer: "ryu",
+    },
+    {
+        id: 92,
+        ask: "りょ",
+        answer: "ryo",
+    },
+]
+
+//show question
+const question = document.getElementById("question-real")
+
+let currentItem = 0;
+window.addEventListener("DOMContentLoaded", function () {
+    showQuestion();
+})
+
+function showQuestion() {
+    const item = QA[currentItem];
+    question.textContent = item.ask;
+}
+
+//check answer
+const btn = document.querySelector(".enter-answer")
+const inPut = document.querySelector(".answer-text")
+
+btn.addEventListener("click", function(){
+    const userAns = document.getElementById("answer-text").value;
+    const currentAnswer = QA[currentItem].answer;
+    if(userAns === currentAnswer){
+        alert("Correct !!")
+        showNextQuestion()
+        emtyInputField()
+
+    } else {
+        alert("Incorrect")
+    }
+});
+
+//next question
+function showNextQuestion(){
+    currentItem = Math.floor(Math.random() * QA.length)
+    showQuestion()
+}
+
+//emty input
+function emtyInputField(){
+    document.getElementById("answer-text").value = "";
+}
+
+
+
